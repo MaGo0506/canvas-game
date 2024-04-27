68496856648f6fffaf737983f415a59412a53918
Marko Goronjic <goronjicmarko24@gmail.com>
2024-04-27 14:38:00 +0200

    - added logic for drawing - created files required for custom types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,38 @@
+'use client'
+
 import { FC } from "react";
-interface pageProps {
+import { useDraw } from "../../hooks/useDraw";
 
-}
+interface pageProps { }
 
-const page: FC<pageProps> = () => {
+const Page: FC<pageProps> = () => {
+
+  const { canvasRef, onMouseDown } = useDraw(drawLine);
+
+  function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
+    const { x: currX, y: currY } = currentPoint;
+    const lineColor = '#000';
+    const lineWidth = 5;
+
+    let startPoint = prevPoint ?? currentPoint;
+    ctx.beginPath();
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = lineColor;
+    ctx.moveTo(startPoint.x, startPoint.y);
+    ctx.lineTo(currX, currY);
+    ctx.stroke();
+
+    ctx.fillStyle = lineColor;
+    ctx.beginPath();
+    ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-200 flex justify-center items-center">
       <canvas
+        ref={canvasRef}
+        onMouseDown={onMouseDown}
         width={750}
         height={750}
         className="border border-black rounded-md"
@@ -15,4 +41,4 @@ const page: FC<pageProps> = () => {
   );
 }
 
-export default page;
+export default Page;
